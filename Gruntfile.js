b1801e9c79930b7ba2d883c7c97d1a84b825b25f
Grant Forrest <a-type@outlook.com>
2015-06-11 14:37:56 -0400

    Fix things up

---
+++ Gruntfile.js
@@ -0,0 +1,28 @@
+'use strict';
+
+module.exports = function (grunt) {
+	var lessFiles = './lib/assets/less/**/*.less';
+
+	grunt.initConfig({
+		watch: {
+			less: {
+				files: lessFiles,
+				tasks: [ 'less:default' ]
+			}
+		},
+		less: {
+			default: {
+				files: {
+					'./assets/css/style.css': './assets/less/style.less'
+				}
+			}
+		}
+
+	});
+
+	grunt.loadNpmTasks('grunt-contrib-watch');
+	grunt.loadNpmTasks('grunt-contrib-less');
+
+	// Register tasks
+	grunt.registerTask('default', [ 'less:default', 'watch:less' ]);
+};
