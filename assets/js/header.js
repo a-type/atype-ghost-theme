cb19331545ff8a38d55815a56f5f512f3ff1f902
Grant Forrest <a-type@outlook.com>
2015-06-17 16:46:23 -0400

    Header image stuff

--- assets/js/header.js
+++ assets/js/header.js
@@ -1,48 +1,37 @@
-// the header processing.js sketch!
+$(function () {
+	var isMobile = window.matchMedia("(max-width: 959px)");
+	if (!isMobile.matches) {
+		var image = $(".clip-image");
+		image.css("width", image.css("height"));
 
-function headerSketch (processing) {
-	var $canvas = $("#headerSketch");
-	var bg;
-	var icon;
-	var circleSize = 0;
-	var masked = false;
-
-	processing.setup = function () {
-		processing.size($canvas.width(), $canvas.height());
-		bg = processing.loadImage(headerImage);
-		icon = processing.loadImage(iconImage);
-	};
-
-	function crop (image, w, h) {
-		return image.get((image.width - w) / 2, (image.height - h) / 2, w, h);
-	}
-
-	processing.draw = function () {
-		if (!masked && icon.loaded && bg.loaded) {
-			masked = true;
-			if (bg.width - icon.width > bg.height - icon.height) {
-				var ratio = bg.height / icon.height;
-				bg.resize(bg.width * ratio, bg.height * ratio);
+		image.on("transitionend", function () {
+			if (image.css("opacity") === "0") {
+				image.hide();
 			}
 			else {
-				var ratio = bg.width / icon.width;
-				bg.resize(bg.width * ratio, bg.height * ratio);
+				image.show();
 			}
-			bg = crop(bg, icon.width, icon.height);
-			bg.updatePixels();
-			bg.mask(icon);
-		}
+		})
 
-		processing.background(255);
-		var size = $canvas.height() / 2;
-		processing.image(bg, ($canvas.width() - size) / 2, ($canvas.height() - size) / 2, size, size);
-	};
-}
+		$(window).mousemove(function (e) {
+			if (image.css("display") !== "none") {
+				var width = $(window).width();
+				var pctX = (e.pageX - (width / 2)) / width;
 
-$(function () {
-	var processingCanvas = document.getElementById("headerSketch");
+				var height = $(window).height();
+				var pctY = (e.pageY - (height / 2)) / height;
 
-	if (processingCanvas) {
-		var p = new Processing(processingCanvas, headerSketch);
+				image.css("background-position-x", (pctX * 50) + "%");
+				image.css("background-position-y", (pctY * 25) + "%");
+			}
+		});
 	}
-})
+
+	var $document = $(document);
+
+	$document.scroll(function () {
+		if ($document.scrollTop()) {
+			$(".layout-container").addClass("scrolled");
+		}
+	});
+});
