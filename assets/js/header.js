5729d83a19f39abdb341c814455b9cb74d2caf23
Grant Forrest <a-type@outlook.com>
2016-03-11 14:32:31 -0500

    remove image js

--- assets/js/header.js
+++ assets/js/header.js
@@ -1,32 +1,4 @@
 $(function () {
-	var isMobile = window.matchMedia("(max-width: 959px)");
-	if (!isMobile.matches) {
-		var image = $(".clip-image");
-		image.css("width", image.css("height"));
-
-		image.on("transitionend", function () {
-			if (image.css("opacity") === "0") {
-				image.hide();
-			}
-			else {
-				image.show();
-			}
-		})
-
-		$(window).mousemove(function (e) {
-			if (image.css("display") !== "none") {
-				var width = $(window).width();
-				var pctX = (e.pageX - (width / 2)) / width;
-
-				var height = $(window).height();
-				var pctY = (e.pageY - (height / 2)) / height;
-
-				image.css("background-position-x", (pctX * 50) + "%");
-				image.css("background-position-y", (pctY * 25) + "%");
-			}
-		});
-	}
-
 	var $document = $(document);
 
 	$document.scroll(function () {
